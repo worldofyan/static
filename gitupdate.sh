eaabf8d638e13ed83cbf972cf3567b6ef37deb7b
worldofyan <ysirjean@gmail.com>
2022-05-24 19:06:14 +0200

    Essai update automatique

---
+++ gitupdate.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+echo "Ajout fichiers (git add --all)..."
+git add --all
+echo "Job done !"
+echo "======================================================"
+echo ""
+
+echo "Commit fichiers (git commit)..."
+git commit
+echo "Job done !"
+echo "======================================================"
+echo ""
+
+echo "Git push..."
+git push
+echo "Job done !"
+echo "======================================================"
+echo ""
+
+
